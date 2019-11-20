37e81ba24a2516c08d9e120b1c11b81ce732e43a
Cacio Renato <caciorentao@gmail.com>
2019-11-20 16:11:07 -0300

    Merge branch 'master' of https://github.com/cacio/livraria
    # Conflicts:
    #	_OLD/index.html
    #	index.html
    #	tpl/_master.htm
    #	tpl/lista-fornecedor.html

---
+++ _OLD/index.php
@@ -0,0 +1,8 @@
+<?php
+print 'aqui';
+
+exit();
+header("location: ./php/home.php");
+
+?>
+ 
--- inc/inc.connect.php
+++ inc/inc.connect.php
@@ -2,5 +2,5 @@
 define('HOST','localhost');
 define('USER','root');
 define('SENHA','');
-define('BD','mydb');
+define('BD','livraria');
 ?>
